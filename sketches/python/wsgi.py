1f29a49ec8456c3b99a3af43bbeec58d90a11487
Michael Austin Simpson <michaelaustinsimpson@Michaels-MacBook-Pro.local>
2020-01-27 11:45:46 -0800

    Jenkins Push to Source Control

--- sketches/python/wsgi.py
+++ sketches/python/wsgi.py
@@ -1,4 +1,4 @@
-import requests
+import os, sys, json
 import cgi
 
 def application(env, start_response):
@@ -7,9 +7,14 @@ def application(env, start_response):
 	('Access-Control-Allow-Origin', '*')
 	])
 
+	data = sys.stdin.read(int(os.environ.get('HTTP_CONTENT_LENGTH', 0)))
+
+	if data:
+		print(list(json.loads(data).keys())) # Prints out keys of json
+
 	form = cgi.FieldStorage().getvalue("firstName")
 	
 	print("raw POST data:")
-	print(request.POST.get("firstName", "default"))
+	print("")
 
 	return [b"HELLO WORLD!"]
