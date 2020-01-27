43e01b13b891e86ddef1e0d5633955460b6beb45
Michael Austin Simpson <michaelaustinsimpson@Michaels-MacBook-Pro.local>
2020-01-27 11:20:46 -0800

    Jenkins Push to Source Control

--- sketches/python/wsgi.py
+++ sketches/python/wsgi.py
@@ -5,6 +5,18 @@ def application(env, start_response):
 	('Content-Type','text/html'),
 	('Access-Control-Allow-Origin', '*')
 	])
+
+	POST={}
+	args=sys.stdin.read().split('&')
+
+	for arg in args: 
+		t=arg.split('=')
+		if len(t)>1: k, v=arg.split('='); POST[k]=v
+
 	form = cgi.FieldStorage().getvalue("firstName")
 	print(form)
-	return [b"Hello World!"]
+	print("BINGO")
+	print("BINGO")
+	print("BINGO")
+	return "HELLO WORLD!"
+	return form
