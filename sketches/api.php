071cf090df7b52d3e3eff111c8e696e7e5d3631f
Michael Austin Simpson <michaelaustinsimpson@Michaels-MacBook-Pro.local>
2020-01-27 14:55:22 -0800

    Jenkins Push to Source Control

--- sketches/api.php
+++ sketches/api.php
@@ -241,7 +241,7 @@
         xhttpThirteen.setRequestHeader("firstName", "Michael");
         xhttpThirteen.setRequestHeader("occupation", "Software Developer");
         console.log(parsedFormDataObject);
-        xhttpThirteen.send();
+        xhttpThirteen.send("fname=Henry&lname=Ford");
 	}
 	
     //HTTP Request State Monitoring & Response Handling Functions 
--- sketches/python/wsgi.py
+++ sketches/python/wsgi.py
@@ -1,7 +1,8 @@
 def application(env, start_response):
 	start_response('200 OK', [
 	('Content-Type','text/html'),
-	('Access-Control-Allow-Origin', '*')
+	('Access-Control-Allow-Origin', '*'),
+	('Access-Control-Allow-Headers', '*')
 	])
 
 	print(env)
