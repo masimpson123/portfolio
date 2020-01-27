740e163b8cf6f5da7ef23cd4977fa9d0802caa9b
Michael Austin Simpson <michaelaustinsimpson@Michaels-MacBook-Pro.local>
2020-01-27 10:00:46 -0800

    Jenkins Push to Source Control

--- sketches/python/wsgi.py
+++ sketches/python/wsgi.py
@@ -1,11 +1,11 @@
 import cgi
 form = cgi.FieldStorage()
 test = form["firstName"]
-return [test]
 
 def application(env, start_response):
 	start_response('200 OK', [
 	('Content-Type','text/html'),
 	('Access-Control-Allow-Origin', '*')
 	])
-	return [b"Hello World!"]
+	#return [b"Hello World!"]
+	return [test]
