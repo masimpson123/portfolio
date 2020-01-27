3a7f1521a147ef257a31501ec96f4e801fb524a8
Michael Austin Simpson <michaelaustinsimpson@Michaels-MacBook-Pro.local>
2020-01-27 14:50:22 -0800

    Jenkins Push to Source Control

--- sketches/python/wsgi.py
+++ sketches/python/wsgi.py
@@ -1,11 +1,10 @@
-import os
-
 def application(env, start_response):
 	start_response('200 OK', [
 	('Content-Type','text/html'),
 	('Access-Control-Allow-Origin', '*')
 	])
 
+	print(env)
 	print(env['QUERY_STRING'])
 
 	return [b"HELLO WORLD!"]
