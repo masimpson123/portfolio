e72bb95f3368d71089cb0bb668dd50260be8faf0
Michael Austin Simpson <michaelaustinsimpson@Michaels-MacBook-Pro.local>
2020-01-27 10:55:46 -0800

    Jenkins Push to Source Control

--- sketches/python/wsgi.py
+++ sketches/python/wsgi.py
@@ -1,27 +1,10 @@
-from cgi import parse_qs
+import cgi
 
 def application(env, start_response):
-
-	try:
-		request_body_size = int(env.get('CONTENT_LENGTH', 0))
-	except (ValueError):
-		request_body_size = 0
-
-	request_body = env['wsgi.input'].read(request_body_size)
-	d = parse_qs(request_body)
-
-	firstName = d.get('firstName', [''])[0]
-	occupation = d.get('occupation', [''])[0]
-
-	response_body = {
-		'firstName': firstName or 'Empty',
-		'occupation': occupation or 'Empty',
-	}
-
 	start_response('200 OK', [
 	('Content-Type','text/html'),
 	('Access-Control-Allow-Origin', '*')
 	])
-
-	#return [b"Hello World!"]
-	return response_body
+	form = cgi.FieldStorage()
+	print(form["firstName"])
+	return [b"Hello World!"]
