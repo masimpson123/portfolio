8b0484a89beb037d25216120159a8dad28fc6a39
Michael Austin Simpson <michaelaustinsimpson@Michaels-MacBook-Pro.local>
2020-01-27 16:20:22 -0800

    Jenkins Push to Source Control

--- sketches/python/wsgi.py
+++ sketches/python/wsgi.py
@@ -1,3 +1,36 @@
+import mysql.connector
+import json
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="theEarth123$",
+  database="python",
+  auth_plugin='mysql_native_password'
+)
+
+mycursor = mydb.cursor()
+
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello World"]
+	start_response('200 OK', [
+	('Content-Type','text/html'),
+	('Access-Control-Allow-Origin', '*'),
+	('Access-Control-Allow-Headers', '*')
+	])
+
+	print("-----")
+	print(env)
+	print(env)
+	print(env)
+	print("-----")
+
+	if "HTTP_FIRSTNAME" in env and "HTTP_OCCUPATION" in env:
+		name = env['HTTP_FIRSTNAME']
+		occupation = env['HTTP_OCCUPATION']
+		sql = "INSERT INTO users (name, occupation) VALUES ('" + name + "','" + occupation + "');"
+		mycursor.execute(sql)
+		mydb.commit()
+		print(mycursor.rowcount, "record inserted.")
+		injectedData = json.dumps([{'name':name,'occupation':occupation}])    
+		injectedDataEncoded = bytes(injectedData, 'utf-8')
+		return [injectedDataEncoded]
