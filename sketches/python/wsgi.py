1384b18bc467275e4f73ffd585922f206d45a926
Michael Austin Simpson <michaelaustinsimpson@Michaels-MacBook-Pro.local>
2020-01-27 16:05:22 -0800

    Jenkins Push to Source Control

--- sketches/python/wsgi.py
+++ sketches/python/wsgi.py
@@ -1,31 +1,30 @@
-#import mysql.connector
-#import json
+import mysql.connector
+import json
 
-#mydb = mysql.connector.connect(
-#  host="localhost",
-#  user="root",
-#  passwd="theEarth123$",
-#  database="python",
-#  auth_plugin='mysql_native_password'
-#)
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="theEarth123$",
+  database="python",
+  auth_plugin='mysql_native_password'
+)
 
-#mycursor = mydb.cursor()
+mycursor = mydb.cursor()
 
 def application(env, start_response):
 	start_response('200 OK', [
 	('Content-Type','text/html'),
-	#('Access-Control-Allow-Origin', '*'),
-	#('Access-Control-Allow-Headers', '*')
+	('Access-Control-Allow-Origin', '*'),
+	('Access-Control-Allow-Headers', '*')
 	])
 
-	#if "HTTP_FIRSTNAME" in env and "HTTP_OCCUPATION" in env:
-		#print("TEST")
-		#name = env['HTTP_FIRSTNAME']
-		#occupation = env['HTTP_OCCUPATION']
-		#sql = "INSERT INTO users (name, occupation) VALUES ('" + name + "','" + occupation + "');"
-		#mycursor.execute(sql)
-		#mydb.commit()
-		#print(mycursor.rowcount, "record inserted.")
-		#injectedData = json.dumps([{'name':name,'occupation':occupation}])    
-		#injectedDataEncoded = bytes(injectedData, 'utf-8') # or test.encode('utf-8')
-		#return [injectedDataEncoded]
+	if "HTTP_FIRSTNAME" in env and "HTTP_OCCUPATION" in env:
+		name = env['HTTP_FIRSTNAME']
+		occupation = env['HTTP_OCCUPATION']
+		sql = "INSERT INTO users (name, occupation) VALUES ('" + name + "','" + occupation + "');"
+		mycursor.execute(sql)
+		mydb.commit()
+		print(mycursor.rowcount, "record inserted.")
+		injectedData = json.dumps([{'name':name,'occupation':occupation}])    
+		injectedDataEncoded = bytes(injectedData, 'utf-8') # or test.encode('utf-8')
+		return [injectedDataEncoded]
