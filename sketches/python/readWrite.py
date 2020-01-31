fd057868d6518938a62d95925f3e9c563de9a145
Michael Austin Simpson <michaelaustinsimpson@Michaels-MacBook-Pro.local>
2020-01-31 11:35:00 -0800

    Jenkins Push to Source Control

--- sketches/python/readWrite.py
+++ sketches/python/readWrite.py
@@ -1,12 +1,20 @@
 def application(env, start_response):
 	start_response('200 OK', [('Content-type', 'text/plain')])
-	print(start_response)
-	print(env)
-	writeToFile()
+	if env["REQUEST_URI"] == "/":
+		writeToFile()
+		readTheFile()
+	else:
+		pass
 
 def writeToFile():
 	import random
 	newContent = str(random.randrange(1,10000))
 	f = open("pythonReadWrite.txt", "a")
 	f.write(newContent + "\n")
+	f.close()
+
+def readTheFile():
+	f = open("demofile.txt", "r")
+	for x in f:
+		print(x)
 	f.close()
