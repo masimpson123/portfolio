8c4d7b8f5fa14c3e58e82ca2ca753f716222bcbe
Michael Austin Simpson <michaelaustinsimpson@Michaels-MacBook-Pro.local>
2020-01-31 11:00:00 -0800

    Jenkins Push to Source Control

--- sketches/python/readWrite.py
+++ sketches/python/readWrite.py
@@ -2,9 +2,20 @@ import random
 
 newContent = str(random.randrange(1,10000))
 
-def application():
-    f = open("pythonReadWrite.txt", "r")
-    #f.write(newContent)
-    for x in f:
-        print(x)
-    f.close()
+def application(env, start_response):
+	start_response('200 OK', [
+	('Content-Type','text/html'),
+	('Access-Control-Allow-Origin', '*'),
+	('Access-Control-Allow-Headers', '*')
+	])
+
+	
+	f = open("pythonReadWrite.txt", "a")
+	f.write(newContent)
+	f.close()
+
+	f.open("pythonReadWrite.txt", "r")
+	f.write(newContent)
+	for x in f:
+		print(x)
+	f.close()
