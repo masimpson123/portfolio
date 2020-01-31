a3d5d59988e2f6894cd96f0b607c5e5fad7f44a2
Michael Austin Simpson <michaelaustinsimpson@Michaels-MacBook-Pro.local>
2020-01-31 11:25:00 -0800

    Jenkins Push to Source Control

--- sketches/python/readWrite.py
+++ sketches/python/readWrite.py
@@ -1,14 +1,12 @@
+import os
+
 def application(env, start_response):
-	start_response('200 OK', [
-	('Content-Type','text/html')
-	])
-	print(env)
+	start_response('200 OK', [('Content-type', 'text/plain')])
+	print(os.environ)
 
 def writeToFile():
 	import random
-
 	newContent = str(random.randrange(1,10000))
-
 	f = open("pythonReadWrite.txt", "a")
 	f.write(newContent + "\n")
 	f.close()
