0cc4ab423bafbb1c253557815036183c8c8ff573
Michael Austin Simpson <michaelaustinsimpson@Michaels-MacBook-Pro.local>
2020-02-01 20:20:41 -0800

    Jenkins Push to Source Control

--- sketches/python/pythonMongoDBTests.py
+++ sketches/python/pythonMongoDBTests.py
@@ -12,20 +12,22 @@ def application(env, start_response):
 		myclient = pymongo.MongoClient("mongodb://localhost:27017/") # connect to mongodb
 		mydb = myclient["mydatabase"] # create/select database
 		mycol = mydb["customers"] # create collection
-		mylist = [
-			{ "name": "Amy", "address": "Apple st 652"},
-			{ "name": "Hannah", "address": "Mountain 21"},
-			{ "name": "Michael", "address": "Valley 345"},
-			{ "name": "Sandy", "address": "Ocean blvd 2"},
-			{ "name": "Betty", "address": "Green Grass 1"},
-			{ "name": "Richard", "address": "Sky st 331"},
-			{ "name": "Susan", "address": "One way 98"},
-			{ "name": "Vicky", "address": "Yellow Garden 2"},
-			{ "name": "Ben", "address": "Park Lane 38"},
-			{ "name": "William", "address": "Central st 954"},
-			{ "name": "Chuck", "address": "Main Road 989"},
-			{ "name": "Viola", "address": "Sideway 1633"}
-		]
-		x = mycol.insert_many(mylist)
-		for x in mycol.find({},{ "_id": 0, "name": 1, "address": 1 }):
+		# mylist = [
+		# 	{ "name": "Amy", "address": "Apple st 652"},
+		# 	{ "name": "Hannah", "address": "Mountain 21"},
+		# 	{ "name": "Michael", "address": "Valley 345"},
+		# 	{ "name": "Sandy", "address": "Ocean blvd 2"},
+		# 	{ "name": "Betty", "address": "Green Grass 1"},
+		# 	{ "name": "Richard", "address": "Sky st 331"},
+		# 	{ "name": "Susan", "address": "One way 98"},
+		# 	{ "name": "Vicky", "address": "Yellow Garden 2"},
+		# 	{ "name": "Ben", "address": "Park Lane 38"},
+		# 	{ "name": "William", "address": "Central st 954"},
+		# 	{ "name": "Chuck", "address": "Main Road 989"},
+		# 	{ "name": "Viola", "address": "Sideway 1633"}
+		# ]
+		# x = mycol.insert_many(mylist)
+		# mydict = { "name": "Peter", "address": "Lowstreet 27" } # data
+		# x = mycol.insert_one(mydict)
+		for x in mycol.find({},{ "_id": 0, "name": 1, "address": 1 }).sort("name", -1):
   			print(x)
